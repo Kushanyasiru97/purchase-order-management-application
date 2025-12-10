494f840ee9a05753df78a919a41aa4cf0f43bd8e
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2025-12-10 12:41:01 +0530

    Purchase order management application UI created

---
+++ PurchaseOrderManagement.UI/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations'; // Add this import
+import { MessageService } from 'primeng/api';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    MessageService
+  ]
+};
