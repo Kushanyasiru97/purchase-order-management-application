a975fb6487f27a6f0b466f17436afce90dd13165
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2025-12-10 10:17:45 +0530

    basic backend crud api created

--- PurchaseOrderManagement.UI/src/app/app.routes.ts
+++ PurchaseOrderManagement.UI/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeScreenComponent } from './pages/home-screen/home-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeScreenComponent,
+        data: { title: 'Purchase Order Management' }
+    },
+    {
+        path: 'purchase-orders',
+        component: HomeScreenComponent,
+        data: { title: 'Purchase Order Management' }
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];
